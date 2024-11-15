31ae7bc2b50e5f002f880ecf798be8c0e09000d2
alexutm123 <pilich.sasha@gmail.com>
2024-11-15 15:29:00 +0300

    feat:img onboarding adn modal "new" adn button

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,12 @@
 import { Text, View, StyleSheet, Button } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
+import { PlantlyButton } from "@/components/PlantlyButton";
 export default function ProfileScreen() {
   const toggleHasOnBoarded = useUserStore((state) => state.toggleHadOnBoarded);
   return (
     <View style={styles.container}>
-      <Button
+      <PlantlyButton
         title="Logout"
         onPress={toggleHasOnBoarded}
       />
--- theme.ts
+++ theme.ts
@@ -1,5 +1,8 @@
 export const theme = {
     spacing12: 12,
+    colorLeafyGreen: "#206a42",
     colorGreen: "#29b365",
+    colorAppleGreen: "#a0d36c",
+    colorLimeGreen: "#d0e57e",
     colorWhite: "#fff",
   };
