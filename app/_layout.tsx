10b7a4a93be88f18b87b04e849a86ff59fb02a0a
alexutm123 <pilich.sasha@gmail.com>
2024-11-20 15:39:02 +0300

    feat:Quick actions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
 import { Stack,SplashScreen } from "expo-router";
-
-export default function RootLayout() {
-  SplashScreen.preventAutoHideAsync();
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
+export default function Layout() {
+  useQuickActionRouting();
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false,animation:"fade" }} />
@@ -10,6 +23,13 @@ export default function RootLayout() {
         options={{
           animation:"fade",
           headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="new"
+        options={{
+          presentation: "modal",
+          title: "New plant",
         }}
       />
     </Stack>
